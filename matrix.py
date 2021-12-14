9d7a7bfbd791f4c0a84afb311851c9958336b627
BenceTuri <96144233+BenceTuri@users.noreply.github.com>
2021-12-14 20:44:38 +0100

    Add files via upload

---
+++ matrix.py
@@ -0,0 +1,10 @@
+time = "time is time lol"
+action = input("Hi, I am an inteligent AI. What the heck dou you want from me?")
+if action == "matrix":
+        while time == "time is time lol":
+            random2 = "01111001 01101111 01110101 00100000 01110000 01101111 01101111 01110000"
+            random3 = "01110111 01101000 01111001 01111001 01101111 01110101 01110010 01100101"
+            random = "01111001 01101111 01110101 00100000 01110011 01110101 01100011 01101011"
+            print (random)
+            print(random2)
+            print(random3)
