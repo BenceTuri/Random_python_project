83a9374801d3a56b43c51f2003c6799b293e011d
BenceTuri <96144233+BenceTuri@users.noreply.github.com>
2022-05-07 15:44:04 +0200

    Update matrix.py

--- matrix.py
+++ matrix.py
@@ -1,6 +1,4 @@
 time = "time is time lol"
-action = input("Hi, I am an inteligent AI. What the heck dou you want from me?")
-if action == "matrix":
         while time == "time is time lol":
             random2 = "01111001 01101111 01110101 00100000 01110000 01101111 01101111 01110000"
             random3 = "01110111 01101000 01111001 01111001 01101111 01110101 01110010 01100101"
