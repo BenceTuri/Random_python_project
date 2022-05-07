ebc575a2eb01e57483da5b333e56cea272646d1f
BenceTuri <96144233+BenceTuri@users.noreply.github.com>
2022-05-07 15:43:27 +0200

    Add files via upload

---
+++ exponencialcalculator.py
@@ -0,0 +1,5 @@
+#egy egyszerű exponenciális számológép
+while 1 < 2:
+    alap = float(input("Mi a hatványalap? \n"))
+    kitevo = float(input("Mi a kitevő? \n"))
+    print(alap ** kitevo)
---
+++ randomnumbergenerator.py
@@ -0,0 +1,5 @@
+import random as r
+while 1 > 0:
+    maxnum = int(input("Mi a legnagyonn szám? "))
+    print(r.randrange(0, maxnum))
+    input("Nyomj meg bármilyen gombot a folytatáshoz!")
